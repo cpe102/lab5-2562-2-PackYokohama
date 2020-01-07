53adfc86587fbdd807b2807df87c21b707b8a804
PackYokohama <59277976+PackYokohama@users.noreply.github.com>
2020-01-07 17:29:07 +0700

    Update lab5_2.cpp

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
+string mixText(string x,string y){
+	string E = "";
+	int i=0;
+	int Q = x.size();
+	int W = y.size();
+	if (Q==W){
+	while(i<Q){                                                           
+	E=E+x[i]+y[i];
+	i++;
+	
+	} 
+return E;
+	}else{
+		return"E";
+	
+  }
+}
 
 
 
