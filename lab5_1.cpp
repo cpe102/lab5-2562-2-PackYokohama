1b0020e5cb2a6a7df8afe658280a99119d3da182
PackYokohama <59277976+PackYokohama@users.noreply.github.com>
2020-01-07 16:04:45 +0700

    Update lab5_1.cpp

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
@@ -7,17 +8,31 @@ int main(){
 	cin >> N;
 	
 	string name[N];
-	int age[N];
-	cout << "Name of student = ";
-	cout << "Age of student = ";
+	int age[N];  
+	int y = 0;
+	while (y < N)
+	{
+		cout << "Name of student"  << y+1 << " =";
+	cin >> name[y] ;
+	cout << "Age of student = "  << y+1 << " =";
+	cin >> age[y] ;
+	y ++;
+	}
 	
 	int key;
-	cout << "--------------------------------------\n";
+	cout << "--------------------------------------n";
 	cout << "Enter an age to search: ";
+	
 	cin >> key;
 	cout << "--------------------------------------\n";
+	y+0;
+	while(y < N)
+	{
 	
-
+	cout << name[y]<< "\n";
+	
+		y ++;
+	}
 	
 	cout << "--------------------------------------\n";
 	
